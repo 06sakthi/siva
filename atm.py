4ad410b50b81fdecf2315cbdcea72b24371f9ace
06sakthi <sivameghna1728@gmail.com>
2024-07-17 15:13:18 +0530

    Add files via upload

--- atm.py
+++ atm.py
@@ -13,69 +13,69 @@ balance=5000
 if pin == password:
     while True:
 
-    print("""
-          1 == balance
-          2 == withdraw balance
-          3 == deposit balance
-          4 == exit
-          """
-          )
+        print("""
+            1 == balance
+            2 == withdraw balance
+            3 == deposit balance
+            4 == exit
+             """
+            )
 
-    try:
-        option = int(input("please enter your choise "))
-    except:
-        print("please enter valid option")
-
-
-    if option == 1:
-        print(f"your current balance is {balance}")
-
-        print("==============================================================")
-        print("==============================================================")
-        print("==============================================================")
-
-    if option == 2:
-
-        withdraw_amount = int(input("please enter withdraw_amount "))
-
-        print("==============================================================")
-        print("==============================================================")
-        print("==============================================================")
-
-        balance = balance = withdraw_amount
-
-        print(f"{withdraw_amount}is debited from your account ")
-
-        print("==============================================================")
-        print("==============================================================")
-
-        print(f"your current balance is {balance}")
-
-        print("==============================================================")
-        print("==============================================================")
+        try:
+            option = int(input("please enter your choise "))
+        except:
+            print("please enter valid option")
 
 
-    if option == 3:
+        if option == 1:
+            print(f"your current balance is {balance}")
 
-        deposit_amount = int(input("please enter deposit_amount"))
+            print("==============================================================")
+            print("==============================================================")
+            print("==============================================================")
 
-        balance = balance + deposit_amount
+        if option == 2:
 
-        print("==============================================================")
-        print("==============================================================")
+            withdraw_amount = int(input("please enter withdraw_amount "))
 
-        print(f"{deposit_amount} is credited to your account")
+            print("==============================================================")
+            print("==============================================================")
+            print("==============================================================")
 
-        print("==============================================================")
-        print("==============================================================")
+            balance = balance = withdraw_amount
 
-        print(f"your updated balance is {balance}")
+            print(f"{withdraw_amount}is debited from your account ")
 
-        print("==============================================================")
-        print("==============================================================")
+            print("==============================================================")
+            print("==============================================================")
 
-    if option == 4:
-       break
+            print(f"your current balance is {balance}")
+
+            print("==============================================================")
+            print("==============================================================")
+
+
+        if option == 3:
+
+            deposit_amount = int(input("please enter deposit_amount"))
+
+            balance = balance + deposit_amount
+
+            print("==============================================================")
+            print("==============================================================")
+
+            print(f"{deposit_amount} is credited to your account")
+
+            print("==============================================================")
+            print("==============================================================")
+
+            print(f"your updated balance is {balance}")
+
+            print("==============================================================")
+            print("==============================================================")
+
+        if option == 4:
+            break
 else:
     print("wrong pin please try again")
         
